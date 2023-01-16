89bc6f4b762c2d0c16b66aad31b65bc1bc64f927
Igor <(igorrds1998@gmail.com)>
2023-01-16 17:32:58 -0300

    changes

--- src/migrations/1673900433714-default.ts
+++ src/migrations/1673900433714-default.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class default1673147454624 implements MigrationInterface {
-    name = 'default1673147454624'
+export class default1673900433714 implements MigrationInterface {
+    name = 'default1673900433714'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "Users" ("id" SERIAL NOT NULL, "name" text NOT NULL, "lastName" text NOT NULL, "email" character varying NOT NULL, CONSTRAINT "PK_16d4f7d636df336db11d87413e3" PRIMARY KEY ("id"))`);
